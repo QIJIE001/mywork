55fc43e03635ed246fdc0c7360b1507d58ae4d97
QIJIE001 <56019976+QIJIE001@users.noreply.github.com>
2020-09-03 11:41:41 +0800

    Add files via upload

---
+++ text3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 11:25:48 2020
+
+@author: tute
+"""
+
+print('篮球计分系统')
+a=1
+
+i=input('请输入篮球队名称：')
+j=input('请输入分数：')
+print( i,'获得',j,'分')
+b=i
+c=j
+while 1:
+    c=b+a
+    
