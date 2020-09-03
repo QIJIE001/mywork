96ad2f349d0d5a2f2d5f0f9805bf254a6b6d8887
QIJIE001 <56019976+QIJIE001@users.noreply.github.com>
2020-09-03 11:25:19 +0800

    Add files via upload

---
+++ text2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 11:10:35 2020
+
+@author: tute
+"""
+
+
+a=[]
+
+while 1:
+    i=input('请输入数值：')
+    a.append(i)
+    print(a)
+    print('将列表逆序输出：')
+    a.reverse()
+    print(a)    
+    
