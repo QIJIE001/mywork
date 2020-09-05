65d0f12a65cde8cdc70969f93530059922facd2d
QIJIE001 <56019976+QIJIE001@users.noreply.github.com>
2020-09-05 12:34:50 +0800

    Update text3.py

--- text3.py
+++ text3.py
@@ -5,14 +5,34 @@ Created on Thu Sep  3 11:25:48 2020
 @author: tute
 """
 
-print('篮球计分系统')
-a=1
 
-i=input('请输入篮球队名称：')
-j=input('请输入分数：')
-print( i,'获得',j,'分')
-b=i
-c=j
+sum1=0
+sum2=0
+a=[]
+b=[]
+
+game1=input('请输入一队的名字：')
+game2=input('请输入二队的名字：')
 while 1:
-    c=b+a
+    c1=int(input('请输入一队的成绩：'))
+    a.append(c1)
+    sum1=sum(a)
+    print(game1,'的成绩为：',sum1)
+    c2=int(input('请输入二队的成绩：'))
+    b.append(c2)
+    sum2=sum(b)
+    print(game2,'的成绩为：',sum2)
+    c=input('继续请回车，结束请输入：game over')
+    if c=='game over':
+        if sum1>sum2:
+            print('球队',game1,'胜利,当前比分为：',sum1,':',sum2)
+        elif sum1 == sum2:
+             print('球队',game2,'与',game2,'平局,当前比分为：',sum1,':',sum2)
+        else:
+            print('球队',game2,'胜利,当前比分为：',sum1,':',sum2)
+        break
+
+
+
+
     
