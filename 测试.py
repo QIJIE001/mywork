0c5a869abb3c315333bcc1987651888cccac1f65
QIJIE001 <56019976+QIJIE001@users.noreply.github.com>
2020-09-09 15:13:57 +0800

    Update 测试.py

--- 测试.py
+++ 测试.py
@@ -1,14 +1,14 @@
 from urllib import request
 import re
-url = "http://www.qiushibaike.com/imgrank/"
-re_jpg = re.compile(r'<img src="(.*?\.jpg)"')
+url = "https://www.ivsky.com/"
+re_jpg = re.compile('<img src="(.*?)".*?>')
 req = request.Request(url, headers={'User-Agent': 'Chrome/4.0 (compatible; MSIE 5.5; Windows NT)'})
 response = request.urlopen(req)
 html = response.read().decode('utf-8')
 img_list = re_jpg.findall(html)
+print(img_list)
 n = 0
 for i in img_list:
     request.urlretrieve("http:"+i, "%s.jpg"%n)
     print('download %s' % i)
     n+=1
-
